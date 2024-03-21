d85953edcb7dd65ccf7059c630771023dffa73b5
Zeljko Lucic <zeljko.lucic99@gmail.com>
2024-03-21 07:52:59 +0100

    Initial commit

---
+++ Tickers/TickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickersApp.swift
+//  Tickers
+//
+//  Created by Zeljko Lucic on 21.3.24..
+//
+
+import SwiftUI
+
+@main
+struct TickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
