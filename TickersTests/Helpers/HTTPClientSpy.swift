1bce689bc1ad58ed520c1ae0870f79eb0be311cd
Zeljko Lucic <zeljko.lucic99@gmail.com>
2024-03-21 23:17:16 +0100

    Move `HTTPClientSpy` to its own separate file

---
+++ TickersTests/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,31 @@
+//
+//  HTTPClientSpy.swift
+//  TickersTests
+//
+//  Created by Zeljko Lucic on 21.3.24..
+//
+
+import Tickers
+import Foundation
+
+final class HTTPClientSpy: HTTPClient {
+    var requestedURLs = [URL]()
+    private var result: Result<(Data, HTTPURLResponse), Error>?
+    
+    func stub(_ result: Result<(Data, HTTPURLResponse), Error>) {
+        self.result = result
+    }
+    
+    func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
+        requestedURLs.append(url)
+        
+        return switch result {
+        case let .success((data, response)):
+            (data, response)
+        case let .failure(error):
+            throw error
+        case .none:
+            (anyData, anyHTTPURLResponse)
+        }
+    }
+}
--- TickersTests/RemoteTickerRepositoryTests.swift
+++ TickersTests/RemoteTickerRepositoryTests.swift
@@ -154,25 +154,5 @@ final class RemoteTickerRepositoryTests: XCTestCase {
         return (sut, client)
     }
     
-    private final class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        private var result: Result<(Data, HTTPURLResponse), Error>?
-        
-        func stub(_ result: Result<(Data, HTTPURLResponse), Error>) {
-            self.result = result
-        }
-        
-        func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
-            requestedURLs.append(url)
-            
-            return switch result {
-            case let .success((data, response)):
-                (data, response)
-            case let .failure(error):
-                throw error
-            case .none:
-                (anyData, anyHTTPURLResponse)
-            }
-        }
-    }
+    
 }
