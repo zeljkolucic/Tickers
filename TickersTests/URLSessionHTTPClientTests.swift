5b03c2436986f13ce1c03f267027bf709030b05d
Zeljko Lucic <zeljko.lucic99@gmail.com>
2024-03-22 00:28:01 +0100

    Add `nil` `data` and `response` to the request error case to make it more explicit

--- TickersTests/URLSessionHTTPClientTests.swift
+++ TickersTests/URLSessionHTTPClientTests.swift
@@ -24,7 +24,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startIntereceptingRequests()
         let url = anyURL
         let error = anyError
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         
         let sut = URLSessionHTTPClient()
         
@@ -44,11 +44,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
         private static var stubs = [URL: Stub]()
         
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error? = nil) {
-            stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error? = nil) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func startIntereceptingRequests() {
@@ -72,6 +74,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         override func startLoading() {
             guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
