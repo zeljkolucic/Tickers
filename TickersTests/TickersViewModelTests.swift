3cb3874a7d90ea0183695eca79d5dc73048cdbe6
Zeljko Lucic <zeljko.lucic99@gmail.com>
2024-03-22 11:08:14 +0100

    `TickersViewModel` does not load upon creation

---
+++ TickersTests/TickersViewModelTests.swift
@@ -0,0 +1,44 @@
+//
+//  TickersViewModelTests.swift
+//  TickersTests
+//
+//  Created by Zeljko Lucic on 22.3.24..
+//
+
+import Tickers
+import XCTest
+
+final class TickersViewModel {
+    private let tickerRepository: TickerRepository
+    
+    init(tickerRepository: TickerRepository) {
+        self.tickerRepository = tickerRepository
+    }
+}
+
+final class TickersViewModelTests: XCTestCase {
+    func test_init_doesNotLoad() {
+        let (_, repository) = makeSUT()
+        
+        XCTAssertEqual(repository.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: TickersViewModel, repository: TickerRepositorySpy) {
+        let repository = TickerRepositorySpy()
+        let sut = TickersViewModel(tickerRepository: repository)
+        trackForMemoryLeaks(repository)
+        trackForMemoryLeaks(sut)
+        return (sut, repository)
+    }
+    
+    private class TickerRepositorySpy: TickerRepository {
+        var loadCallCount = 0
+        
+        func load() async throws -> [Ticker] {
+            loadCallCount += 1
+            return []
+        }
+    }
+}
