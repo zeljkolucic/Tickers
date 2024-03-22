7d3b0c51dc0b91530b92d77744e4a130b26c0929
Zeljko Lucic <zeljko.lucic99@gmail.com>
2024-03-22 11:35:27 +0100

    Move `TickersEndpoint` to its own separate file

--- TickersTests/TickersEndpointTests.swift
+++ TickersTests/TickersEndpointTests.swift
@@ -5,25 +5,8 @@
 //  Created by Zeljko Lucic on 22.3.24..
 //
 
+import Tickers
 import XCTest
-
-enum TickersEndpoint {
-    case get([String])
-    
-    public func url(from baseURL: URL) -> URL {
-        switch self {
-        case let .get(symbols):
-            var components = URLComponents()
-            components.scheme = baseURL.scheme
-            components.host = baseURL.host
-            components.path = baseURL.path + "/v2/tickers"
-            components.queryItems = [
-                URLQueryItem(name: "symbols", value: symbols.joined(separator: ","))
-            ].compactMap { $0 }
-            return components.url!
-        }
-    }
-}
 
 final class TickersEndpointTests: XCTestCase {
     func test_get_deliversURLFromBaseURL() {
