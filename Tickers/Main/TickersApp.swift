5ff7b7501d069d1c6a823fe9464322a0b3d6b9c0
Zeljko Lucic <zeljko.lucic99@gmail.com>
2024-03-23 17:56:31 +0100

    Embed `TickersListView` inside `TabView`

--- Tickers/Main/TickersApp.swift
+++ Tickers/Main/TickersApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct TickersApp: App {
+    @State private var selectedIndex = 0
+    
     private let baseURL = URL(string: "https://api-pub.bitfinex.com")!
     private let timeInterval: TimeInterval = 1
     
@@ -16,7 +18,13 @@ struct TickersApp: App {
         WindowGroup {
             let url = TickersEndpoint.get(Symbol.allCases.map { $0.rawValue }).url(from: baseURL)
             let viewModel = TickersViewModel(tickerRepository: RemoteTickerRepository(client: URLSessionHTTPClient(), url: url), timeInterval: timeInterval)
-            TickersListView(viewModel: viewModel)
+            TabView(selection: $selectedIndex) {
+                TickersListView(viewModel: viewModel)
+                    .tabItem {
+                        Label("Tickers", systemImage: selectedIndex == 0 ? "chart.bar.fill" : "chart.bar")
+                    }
+            }
+            .tint(.primaryGreen)
         }
     }
 }
